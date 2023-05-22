3896a005a0f1eb14ee55b32f0f44b217d387b430
frankschweitzer <schwfj20@wfu.edu>
2023-05-22 14:13:43 -0400

    flask app

--- app.py
+++ app.py
@@ -1,12 +1,37 @@
-def main():
-    board = [['H', 'J', 'L'],
-            ['O', 'I', 'L'],
-            ['W', 'J', 'M']]
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+
+def main(board):
+    print("Main called")
     words = find_words(board)
+    print("Words found")
     english_words, word_map = get_english_words()
-    valid_words = check_words(words, english_words)
-    valid_words.sort()
-    print(valid_words)
+    print("Got english words")
+    valid_words = check_words(words, word_map)
+    print("Retrieved valid words")
+    return valid_words
+    
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/process', methods=['POST'])
+def process():
+    grid = [[request.form.get(f'row-{i}-col-{j}') for j in range(4)] for i in range(4)]
+    # Process the grid or perform any required operations
+    
+    # Example: Print the grid
+    for row in grid:
+        print(row)
+        
+    valid_words = main(grid)
+    # Return a response or redirect as needed
+    return render_template('results.html', valid_words=valid_words)
+
 
 # Obtain list of all english words
 def get_english_words():
@@ -26,12 +51,13 @@ def get_english_words():
     return word_list, map
 
 # Return words in english language - words that have numbers in them may not work as well
-def check_words(list, english):
-    valid = []
-    for word in list:
-        if word in english and word not in valid:
-            valid.append(word)
+def check_words(word_list, english_words):
+    valid = set()
+    for word in word_list:
+        if word in english_words:
+            valid.add(word)
     return valid
+
 
 # Function to perform DFS on the grid
 def dfs(grid, i, j, visited, word, words):
@@ -73,4 +99,5 @@ def find_words(grid):
     return words
 
 
-main()
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
